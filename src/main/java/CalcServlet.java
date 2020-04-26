8a84079492c49400ed39befbbc7fa54267b86aa8
m_chodorowski <mic.chodorowski@gmail.com>
2020-04-26 20:46:59 +0200

    redirect na klika do formularza, czyszczenie histori wynikow kalkulatora, dzialanie kalkulatora

--- src/main/java/CalcServlet.java
+++ src/main/java/CalcServlet.java
@@ -5,11 +5,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.ArrayList;
 
 @WebServlet(name = "CalcServlet", urlPatterns = "/CalcServlet")
 public class CalcServlet extends HttpServlet {
+    ArrayList<String> calculationHistory = new ArrayList<>();
 
     @Override
     public void init() throws ServletException {
@@ -26,44 +26,57 @@ public class CalcServlet extends HttpServlet {
     }
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String calcResult = "";
+        if (request.getParameter("number1") != null && request.getParameter("number2") != null) {
+            calcResult = calculate(request);
+        } else {
+            int calcHistorySize = calculationHistory.size();
+            calcResult = calculationHistory.get(calcHistorySize-1);
+            calculationHistory = new ArrayList<>();
+        }
+        String goToCalc = "http://localhost:8082/webapp1_war_exploded/index.jsp";
+        String gotoCalcServlet = "http://localhost:8082/webapp1_war_exploded/CalcServlet";
         response.setContentType("text/html;charset=UTF-8");
-        String calcResult = calculate(request);
         try (PrintWriter printWriter = response.getWriter()) {
-            printWriter.println("<h1>Wynik kalkulacji:" + calcResult + "</h1>");
-
+            printWriter.println("<a href=" + goToCalc + "> Wróć do kalkulatora</a>");
+            printWriter.println("<a href=" + gotoCalcServlet + ">Wyczyść historię obliczeń</a>");
+            printWriter.println("<h1>Wynik obliczeń:</h1>");
+            printWriter.println(calcResult);
+            printWriter.println("<h1>Historia obliczeń:");
+            for (int i = 0; i < calculationHistory.size(); i++) {
+                printWriter.println("<br/> " + calculationHistory.get(i));
+            }
         } catch (Exception ex) {
 
         }
     }
 
     private String calculate(HttpServletRequest request) {
-//        try {
-            double number1 = Double.parseDouble(request.getParameter("number1"));
-            double number2 = Double.parseDouble(request.getParameter("number2"));
-            String operation = request.getParameter("operations");
-            String value = number1 + operation + number2 + " = ";
-            double res = 0;
-            switch (operation) {
-                case "+":
-                    value += (number1 + number2);
-                    break;
-                case "-":
-                    value += (number1 - number2);
-                    break;
-                case "*":
-                    value += (number1 * number2);
-                    break;
-                case "/":
-                    if (number2 <= 0.1e-10) value += "Dzielenie przez 0 jest niemozliwe";
-                    else
-                        value += (number1 / number2);
-                    break;
-                default:
-                    value += (number1 + number2);
-            }
-            return value;
-//        } catch (NumberFormatException | NullPointerException ex) {
-//            return "Bledne dane";
-//        }
+        double number1 = Double.parseDouble(request.getParameter("number1"));
+        double number2 = Double.parseDouble(request.getParameter("number2"));
+        String operation = request.getParameter("operations");
+        String value = number1 + operation + number2 + " = ";
+        double res = 0;
+        switch (operation) {
+            case "-":
+                value += (number1 - number2);
+                break;
+            case "*":
+                value += (number1 * number2);
+                break;
+            case "/":
+                if (number2 <= 0.1e-10) value += "Dzielenie przez 0 jest niemozliwe";
+                else
+                    value += (number1 / number2);
+                break;
+            default:
+                value += (number1 + number2);
+        }
+        calculationHistory.add(value);
+        return value;
+    }
+
+    private void deleteCalculationHistory() {
+        calculationHistory = new ArrayList<>();
     }
 }
